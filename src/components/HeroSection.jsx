f5a94eab9e282540409315ef9fc83e41957c991b
mdazmi28 <md.azmi.siddique@gmail.com>
2025-02-19 10:32:50 +0600

    v0.0.10 Hero UI

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
 
     return (
         <div className='w-full overflow-hidden'>
-            <div className='w-full h-full bg-[url(/assets/images/hero_image.png)] bg-cover bg-bottom bg-no-repeat absolute inset-0'>
+            <div className='w-full h-full bg-[url(/assets/images/hero_image.png)] dark:bg-[url(/assets/images/dark_hero.png)] bg-cover bg-bottom bg-no-repeat absolute inset-0'>
                 <div className='flex flex-col-reverse md:flex-row h-full'>
                     <div className='md:w-1/2 flex flex-col gap-4 items-center justify-start'>
                         <img src="/assets/images/logo.png" alt="Image" className='w-20 h-20 md:w-80 md:h-80' />
--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ServicesSection = () => {
     return (
         <div className='w-full overflow-hidden'>
-        <div className='w-full h-full bg-[url(/assets/images/section_image.png)] bg-cover bg-bottom bg-no-repeat absolute inset-0'>
+        <div className='w-full h-full bg-[url(/assets/images/section_image.png)] dark:bg-[url(/assets/images/dark_section.png)] bg-cover bg-bottom bg-no-repeat absolute inset-0'>
             <div className='flex flex-col-reverse md:flex-row h-full'>
                Hello
             </div>
