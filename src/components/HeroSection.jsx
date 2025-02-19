54b9f34dd259c6b9b76342776b1e08155c20973c
mdazmi28 <md.azmi.siddique@gmail.com>
2025-02-19 10:50:31 +0600

    v0.0.12 responsive view update

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,10 +7,10 @@ const HeroSection = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     return (
-        <div className='w-full overflow-hidden'>
+        
             <div className='w-full h-full bg-[url(/assets/images/hero_image.png)] dark:bg-[url(/assets/images/dark_hero.png)] bg-cover bg-bottom bg-no-repeat absolute inset-0'>
                 <div className='flex flex-col-reverse md:flex-row h-full'>
-                    <div className='md:w-1/2 flex flex-col gap-4 items-center justify-start'>
+                    <div className='md:w-1/2 flex flex-col gap-4 items-center justify-start p-6'>
                         <img src="/assets/images/logo.png" alt="Image" className='w-20 h-20 md:w-80 md:h-80' />
                         <h1 className='text-2xl'>The future is not something we enter, but something we create.</h1>
                         <button onClick={()=>setIsModalOpen(true)} htmlFor="my_modal_7" className="px-8 py-3 text-black font-bold uppercase bg-white border-2 border-black rounded-full hover:bg-black hover:text-white transition-all duration-300">
@@ -22,7 +22,7 @@ const HeroSection = () => {
                         <LottieAnimation />
                     </div>
                 </div>
-            </div>
+         
             <ContactModal 
                 isOpen={isModalOpen} 
                 onClose={() => setIsModalOpen(false)} 
