140656cce0de597c2367b58feae3da745cd2eafb
mdazmi28 <md.azmi.siddique@gmail.com>
2025-02-19 10:38:30 +0600

    v0.0.11 modal opening

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -13,7 +13,7 @@ const HeroSection = () => {
                     <div className='md:w-1/2 flex flex-col gap-4 items-center justify-start'>
                         <img src="/assets/images/logo.png" alt="Image" className='w-20 h-20 md:w-80 md:h-80' />
                         <h1 className='text-2xl'>The future is not something we enter, but something we create.</h1>
-                        <button htmlFor="my_modal_7" className="px-8 py-3 text-black font-bold uppercase bg-white border-2 border-black rounded-full hover:bg-black hover:text-white transition-all duration-300">
+                        <button onClick={()=>setIsModalOpen(true)} htmlFor="my_modal_7" className="px-8 py-3 text-black font-bold uppercase bg-white border-2 border-black rounded-full hover:bg-black hover:text-white transition-all duration-300">
                             CONTACT US
                         </button>
 
