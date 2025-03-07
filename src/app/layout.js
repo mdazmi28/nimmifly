71a70a56a5fe1a4c0bf37f0ea5166c5021f8acb2
md-azmi-siddique <md.azmi.siddique@gmail.com>
2025-03-07 19:44:30 +0600

    look good

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body className="min-h-screen">
-        <div className='relative min-h-screen w-full bg-gray-50 dark:bg-gray-900'>
+        <div className='relative min-h-screen w-full bg-gray-100 dark:bg-gray-900'>
           {/* Navbar Container */}
           <div className='fixed top-0 left-0 right-0 h-[50px] flex justify-center items-center z-50'>
             <Navbar />
